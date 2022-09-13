815867cb7516d867c4ea834b2408872f85e11dfb
Mykaelsss <mykaelsicard04@gmail.com>
2022-09-13 15:40:40 -0400

    initial commit web development

---
+++ Week one/Day 2/algorithms.js
@@ -0,0 +1,28 @@
+/**
+ * Concatenate three given strings together in this order:
+ * 3rd string, 1st string, 2nd string.
+ * @returns {string} The three given strings combined (concatenated).
+ */
+
+
+/**
+ * Combine two given strings together with the smaller string being added
+ * in the front. If they are the same length, the first string will be
+ * added to the front.
+ * @returns {string} The combined strings.
+ */
+ function combineSmallerStringFirst(/* Replace this comment with the needed parameters. What info does this function need? */) {
+   // Replace this comment with the code needed to make this function work.
+ }
+
+/**
+ * Given a string and an integer representing how many times the string should
+ * be repeated, create a new string that is the given string repeated that
+ * many times.
+ * @param {string} text The given string to be repeated.
+ * @param {number} num The number of times to repeat the given string.
+ * @returns {string} The given string repeated the specified amount of times.
+ */
+ function stringRepeat( text, num ) {
+
+ }
